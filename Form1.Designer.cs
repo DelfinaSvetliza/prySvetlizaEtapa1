58cfcbf1a332b45da858431ca38936d26d1208e9
DelfinaSvetliza <delfina.svetliza@gmail.com>
2024-04-28 17:52:53 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace prySvetlizaEtapa1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.pctFirma = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(131, 380);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(190, 58);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // pctFirma
+            // 
+            this.pctFirma.Location = new System.Drawing.Point(60, 28);
+            this.pctFirma.Name = "pctFirma";
+            this.pctFirma.Size = new System.Drawing.Size(336, 312);
+            this.pctFirma.TabIndex = 1;
+            this.pctFirma.TabStop = false;
+            this.pctFirma.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseDown);
+            this.pctFirma.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseMove);
+            this.pctFirma.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pctFirma_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 509);
+            this.Controls.Add(this.pctFirma);
+            this.Controls.Add(this.btnGuardar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pctFirma)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.PictureBox pctFirma;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace prySvetlizaEtapa1
+{
+    public partial class Form1 : Form
+    {
+        private Point previousPoint;
+        private bool isMouseDrawing = false;
+        private Bitmap signatureBitmap;
+        private string fondo;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSignaturePictureBox();
+            pctFirma.MouseDown += new MouseEventHandler(pctFirma_MouseDown);
+            pctFirma.MouseMove += new MouseEventHandler(pctFirma_MouseMove);
+            pctFirma.MouseUp += new MouseEventHandler(pctFirma_MouseUp);
+
+        }
+        private void InitializeSignaturePictureBox()
+        {
+            signatureBitmap = new Bitmap(pctFirma.Width, pctFirma.Height);
+            pctFirma.Image = signatureBitmap;
+            Graphics g = Graphics.FromImage(pctFirma.Image);
+            g.Clear(Color.White);
+            g.Dispose();
+        }
+
+        private void pctFirma_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isMouseDrawing = true;
+                previousPoint = e.Location;
+            }
+        }
+
+        private void pctFirma_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDrawing)
+            {
+                using (Graphics g = Graphics.FromImage(pctFirma.Image))
+                {
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    using (Pen pen = new Pen(Color.Black, 2))
+                    {
+                        g.DrawLine(pen, previousPoint, e.Location);
+                    }
+                }
+                previousPoint = e.Location;
+                pctFirma.Invalidate();
+            }
+        }
+
+        private void pctFirma_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDrawing = false;
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
